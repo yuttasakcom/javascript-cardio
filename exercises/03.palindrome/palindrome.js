f6da5fc3c988187a673691a8d6af55eb98a56d26
YoProgrammer <yuttasakcom@gmail.com>
2018-03-14 17:27:24 +0700

    create 03.palindrome

--- exercises/03.palindrome/palindrome.js
+++ exercises/03.palindrome/palindrome.js
@@ -1,3 +1,11 @@
-function palindrome () {}
+// Solution 1 # compair reverse
+function palindrome (str) {
+  return str === str.split('').reverse().join('')
+}
+
+// Solution 2 # every
+// function palindrome (str) {
+//   return str.split('').every((char, i) => char === str[str.length - i - 1])
+// }
 
 module.exports = palindrome
--- exercises/03.palindrome/palindrome.test.js
+++ exercises/03.palindrome/palindrome.test.js
@@ -4,3 +4,27 @@ const palindrome = require('./palindrome')
 test('Palindrome function is defined', () => {
   expect(typeof palindrome).toEqual('function')
 })
+
+test('"aba" is a palindrome', () => {
+  expect(palindrome('aba')).toBeTruthy()
+})
+
+test('" aba" is not a palindrome', () => {
+  expect(palindrome(' aba')).toBeFalsy()
+})
+
+test('"greeting" is not a palindrome', () => {
+  expect(palindrome('greeting')).toBeFalsy()
+})
+
+test('"1000000001" is a palindrome', () => {
+  expect(palindrome('1000000001')).toBeTruthy()
+})
+
+test('"Fish hsif" is not a palindrome', () => {
+  expect(palindrome('Fish hsif')).toBeFalsy()
+})
+
+test('"pennep" is a palindrome', () => {
+  expect(palindrome('pennep')).toBeTruthy()
+})
