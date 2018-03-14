0d461db64d67bb4bfd9acc976294823b2cfd6f46
YoProgrammer <yuttasakcom@gmail.com>
2018-03-14 17:58:18 +0700

    create 04.maxchar

--- exercises/04.maxchar/maxchar.test.js
+++ exercises/04.maxchar/maxchar.test.js
@@ -2,6 +2,19 @@
 
 const maxchar = require('./maxchar')
 
-test.only('maxchar function exists', () => {
+test('maxchar function exists', () => {
   expect(typeof maxchar).toEqual('function')
+})
+
+test('Finds the most frequently used char', () => {
+  expect(maxchar('a')).toEqual('a')
+  expect(maxchar('abcdefghijklmnaaaaa')).toEqual('a')
+})
+
+test('Works with numbers in the string', () => {
+  expect(maxchar('ab1c1d1e1f1g1')).toEqual('1')
+})
+
+test('"abcccccccd" maxchar is c', () => {
+  expect(maxchar('abcccccccd')).toEqual('c')
 })
