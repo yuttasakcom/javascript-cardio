cf77eaacc444ab0b81190caac741649d0c05cdcf
YoProgrammer <yuttasakcom@gmail.com>
2018-04-20 23:40:42 +0700

    create 08.capitalization

--- exercises/08.capitalization/capitalize.js
+++ exercises/08.capitalization/capitalize.js
@@ -1,15 +1,25 @@
-const capitalization = str => {
-  let result = str[0].toUpperCase();
+const capitalize = str => {
+  const words = [];
 
-  for (let i = 1; i < str.length; i++) {
-    if (str[i - 1] === " ") {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i];
-    }
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
   }
 
-  return result;
+  return words.join(" ");
 };
 
-module.exports = capitalization;
+// const capitalize = str => {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// };
+
+module.exports = capitalize;
